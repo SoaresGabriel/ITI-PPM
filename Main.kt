baf4f5f87d99f61dc8fadc55cf35b93854efb530
Gabriel Soares <soaresggm@gmail.com>
2018-05-12 19:43:44 -0300

    Change Main read info from command line and save context order in compressed file

--- Main.kt
+++ Main.kt
@@ -3,36 +3,45 @@ import arithmeticEncoder.ArithmeticEncoder
 import arithmeticEncoder.BitInputStream
 import arithmeticEncoder.BitOutputStream
 import java.io.*
+import kotlin.system.measureTimeMillis
 
 
 fun main(args: Array<String>) {
-    val inputFile = File("test.txt")
-    val outputFile = File("test.txt.PPM")
+    val time = measureTimeMillis {
 
-    val operation = 'd'
-    val maxContextOrder = 2
+        val op = args[0][1]
+        when(op) {
 
-    when(operation) {
-        'c' -> {
-            BufferedInputStream(FileInputStream(inputFile)).use { input ->
-                BitOutputStream(BufferedOutputStream(FileOutputStream(outputFile))).use { out ->
-                    compress(input, out, maxContextOrder)
-                }
+            'c' -> {
+                println("Comprimindo...")
+                if (args.size != 3) throw IllegalArgumentException("Arguments: [-c k | -d] file")
+
+                val maxContextOrder = args[1].toInt()
+                val filename = args[2]
+                compress(filename, maxContextOrder)
             }
+
+            'd' -> {
+                println("Descomprimindo...")
+                if (args.size != 2) throw IllegalArgumentException("Arguments: [-c k | -d] file")
+
+                val filename = args[1]
+                decompress(filename)
+            }
+
+            else -> throw IllegalArgumentException("Invalid operation. Arguments: [-c k | -d] file")
         }
 
-        'd' -> {
-            BitInputStream(FileInputStream(inputFile)).use { input ->
-                BufferedOutputStream(FileOutputStream(outputFile)).use { out ->
-                    decompress(input, out, maxContextOrder)
-                }
-            }
-        }
     }
 
+    println("t: $time ms")
 }
 
-fun compress(input: InputStream, output: BitOutputStream, maxContextOrder: Int) {
+fun compress(filename: String, maxContextOrder: Int) {
+    val input = BufferedInputStream(FileInputStream(File(filename)))
+    val output = BitOutputStream(BufferedOutputStream(FileOutputStream(File("$filename.ppm"))))
+
+    output.write8BitInt(maxContextOrder)
 
     val encoder = ArithmeticEncoder(output)
     val ppmEncoder = PpmEncoder(encoder, maxContextOrder)
@@ -48,19 +57,43 @@ fun compress(input: InputStream, output: BitOutputStream, maxContextOrder: Int) 
     ppmEncoder.encode(256) // EOF
 
     encoder.finish()
+    input.close()
+    output.close()
 }
 
-fun decompress(input: BitInputStream, output: OutputStream, maxContextOrder: Int) {
+fun decompress(filename: String) {
+    if(!filename.endsWith(".ppm"))
+        throw IllegalArgumentException("Not a .ppm file")
+
+    val input = BitInputStream(FileInputStream(File(filename)))
+    val output = BufferedOutputStream(FileOutputStream(filename.removeSuffix(".ppm") + '2'))
+
+    val maxContextOrder = input.read8BitInt()
 
     val decoder = ArithmeticDecoder(input)
     val ppmDecoder = PpmDecoder(decoder, maxContextOrder)
 
     while(true) {
         val symbol = ppmDecoder.decode()
-        if(symbol == 256) return
+        if(symbol == 256) break
 
         output.write(symbol)
     }
 
+    output.close()
+    input.close()
 }
 
+fun BitOutputStream.write8BitInt(int: Int) {
+    for(i in 0 until 8) {
+        this.write((int shr (7-i)) and 1)
+    }
+}
+
+fun BitInputStream.read8BitInt(): Int {
+    var int = 0
+    for(i in 0 until 8) {
+        int = (int shl 1) or this.read()
+    }
+    return int
+}
